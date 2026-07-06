1dcd0a7d9ac121d96d81c30ef5788bcf492527bd
NKA-Events <nkartistryevents@gmail.com>
2026-07-06 17:44:50 +0800

    Add files via upload
    Updated

--- guest-data.js
+++ guest-data.js
@@ -1,22 +1,602 @@
 const guestData = [
   {
-    "name": "H Kodikara",
-    "phone": "449",
-    "table": "1"
-  },
-  {
-    "name": "S De Silva",
-    "phone": "44",
-    "table": "2"
-  },
-  {
-    "name": "N K",
+    "name": "Ajith Mathota",
     "phone": "",
     "table": "1"
   },
   {
-    "name": "G K",
-    "phone": "4444",
+    "name": "Kumuduni Mathota",
+    "phone": "",
+    "table": "1"
+  },
+  {
+    "name": "Suresh Punchihewa",
+    "phone": "",
+    "table": "1"
+  },
+  {
+    "name": "Achala Punchihewa",
+    "phone": "",
+    "table": "1"
+  },
+  {
+    "name": "Dinu Ekanayaka",
+    "phone": "",
+    "table": "1"
+  },
+  {
+    "name": "Nirma Ekanayaka",
+    "phone": "",
+    "table": "1"
+  },
+  {
+    "name": "Maharu Mendis",
+    "phone": "",
+    "table": "1"
+  },
+  {
+    "name": "Maduri De Silva",
+    "phone": "",
+    "table": "1"
+  },
+  {
+    "name": "Sidath Parakramawansa",
+    "phone": "",
+    "table": "1"
+  },
+  {
+    "name": "Inoka Parakramawansa",
+    "phone": "",
+    "table": "1"
+  },
+  {
+    "name": "Rohan Mendis",
+    "phone": "",
     "table": "2"
+  },
+  {
+    "name": "Thanuja Mendis",
+    "phone": "",
+    "table": "2"
+  },
+  {
+    "name": "Namal Thennakoon",
+    "phone": "",
+    "table": "2"
+  },
+  {
+    "name": "Chaya Warusapperuma",
+    "phone": "",
+    "table": "2"
+  },
+  {
+    "name": "Chandana Thilake",
+    "phone": "",
+    "table": "2"
+  },
+  {
+    "name": "Dimuthu Peterson",
+    "phone": "",
+    "table": "2"
+  },
+  {
+    "name": "Mahesh Bandara",
+    "phone": "",
+    "table": "2"
+  },
+  {
+    "name": "Shakila Basnayaka",
+    "phone": "",
+    "table": "2"
+  },
+  {
+    "name": "Hirosha Ranasingha",
+    "phone": "",
+    "table": "2"
+  },
+  {
+    "name": "Inosha Ranasingha",
+    "phone": "",
+    "table": "2"
+  },
+  {
+    "name": "Muditha De Silva",
+    "phone": "",
+    "table": "3"
+  },
+  {
+    "name": "Sevvandi De Silva",
+    "phone": "",
+    "table": "3"
+  },
+  {
+    "name": "Upali Nawarathna",
+    "phone": "",
+    "table": "3"
+  },
+  {
+    "name": "Anushka Nawarathna",
+    "phone": "",
+    "table": "3"
+  },
+  {
+    "name": "Anesley Fernando",
+    "phone": "",
+    "table": "3"
+  },
+  {
+    "name": "Duthika Fernando",
+    "phone": "",
+    "table": "3"
+  },
+  {
+    "name": "Shan Peris",
+    "phone": "",
+    "table": "3"
+  },
+  {
+    "name": "Samantha Hendalage",
+    "phone": "",
+    "table": "3"
+  },
+  {
+    "name": "Chandika Dissanayaka",
+    "phone": "",
+    "table": "3"
+  },
+  {
+    "name": "Dilrukshi Wijesinghe",
+    "phone": "",
+    "table": "3"
+  },
+  {
+    "name": "Amila Jayalath",
+    "phone": "",
+    "table": "4"
+  },
+  {
+    "name": "Yasoma Adikari",
+    "phone": "",
+    "table": "4"
+  },
+  {
+    "name": "Rajitha Namarathna",
+    "phone": "",
+    "table": "4"
+  },
+  {
+    "name": "Dilini Gurusinghe",
+    "phone": "",
+    "table": "4"
+  },
+  {
+    "name": "Chandima Rajasekara",
+    "phone": "",
+    "table": "4"
+  },
+  {
+    "name": "Hemanthi Rajasekara",
+    "phone": "",
+    "table": "4"
+  },
+  {
+    "name": "Kids 1",
+    "phone": "",
+    "table": "4"
+  },
+  {
+    "name": "Kida 2",
+    "phone": "",
+    "table": "4"
+  },
+  {
+    "name": "Kids 3",
+    "phone": "",
+    "table": "4"
+  },
+  {
+    "name": "Kida 4",
+    "phone": "",
+    "table": "4"
+  },
+  {
+    "name": "Thewmini Madurapperuma",
+    "phone": "",
+    "table": "5"
+  },
+  {
+    "name": "Methu Ahabadagedara",
+    "phone": "",
+    "table": "5"
+  },
+  {
+    "name": "Shakya Thennakoon",
+    "phone": "",
+    "table": "5"
+  },
+  {
+    "name": "Sayumi Wanninayaka",
+    "phone": "",
+    "table": "5"
+  },
+  {
+    "name": "Seneka Wijesekara",
+    "phone": "",
+    "table": "5"
+  },
+  {
+    "name": "Saveesha Wickramasinghe",
+    "phone": "",
+    "table": "5"
+  },
+  {
+    "name": "Okithma Ranasinghe",
+    "phone": "",
+    "table": "5"
+  },
+  {
+    "name": "Dinadi Walawage",
+    "phone": "",
+    "table": "5"
+  },
+  {
+    "name": "Anaya Singh",
+    "phone": "",
+    "table": "5"
+  },
+  {
+    "name": "Vinu Dikovita",
+    "phone": "",
+    "table": "5"
+  },
+  {
+    "name": "Parakrama Sumanarathna",
+    "phone": "",
+    "table": "6"
+  },
+  {
+    "name": "Thanuja Sumanarathna",
+    "phone": "",
+    "table": "6"
+  },
+  {
+    "name": "Nishantha Madurapperuma",
+    "phone": "",
+    "table": "6"
+  },
+  {
+    "name": "Thushari Madurapperuma",
+    "phone": "",
+    "table": "6"
+  },
+  {
+    "name": "Duminda Wimalarathna",
+    "phone": "",
+    "table": "6"
+  },
+  {
+    "name": "Jayamali Wijerathna",
+    "phone": "",
+    "table": "6"
+  },
+  {
+    "name": "Dammika Devapriya",
+    "phone": "",
+    "table": "6"
+  },
+  {
+    "name": "Thushani Devapriya",
+    "phone": "",
+    "table": "6"
+  },
+  {
+    "name": "Dissa Rathnayaka",
+    "phone": "",
+    "table": "6"
+  },
+  {
+    "name": "Indika Denawaka",
+    "phone": "",
+    "table": "6"
+  },
+  {
+    "name": "Sudharman Samarakoon",
+    "phone": "",
+    "table": "7"
+  },
+  {
+    "name": "Ajitha Wanninayaka",
+    "phone": "",
+    "table": "7"
+  },
+  {
+    "name": "Priyani Wanninayaka",
+    "phone": "",
+    "table": "7"
+  },
+  {
+    "name": "Ajith Weerakoon",
+    "phone": "",
+    "table": "7"
+  },
+  {
+    "name": "Kanthi Hettiarachchi",
+    "phone": "",
+    "table": "7"
+  },
+  {
+    "name": "Ravi Samarasinghe",
+    "phone": "",
+    "table": "7"
+  },
+  {
+    "name": "Mirani Kehelpannala",
+    "phone": "",
+    "table": "7"
+  },
+  {
+    "name": "Jayanthi Gamage",
+    "phone": "",
+    "table": "7"
+  },
+  {
+    "name": "Nirosha Abesekara",
+    "phone": "",
+    "table": "7"
+  },
+  {
+    "name": "?",
+    "phone": "",
+    "table": "7"
+  },
+  {
+    "name": "Asela Bulathsinhala",
+    "phone": "",
+    "table": "8"
+  },
+  {
+    "name": "Varuni De Silva",
+    "phone": "",
+    "table": "8"
+  },
+  {
+    "name": "Chanaka De Silva",
+    "phone": "",
+    "table": "8"
+  },
+  {
+    "name": "Udeni De Silva",
+    "phone": "",
+    "table": "8"
+  },
+  {
+    "name": "Thilina Zoysa",
+    "phone": "",
+    "table": "8"
+  },
+  {
+    "name": "Nilakshi Zoysa",
+    "phone": "",
+    "table": "8"
+  },
+  {
+    "name": "Mahendra Sagara",
+    "phone": "",
+    "table": "8"
+  },
+  {
+    "name": "Shyama Fernando",
+    "phone": "",
+    "table": "8"
+  },
+  {
+    "name": "Chithral Jayasinghe",
+    "phone": "",
+    "table": "8"
+  },
+  {
+    "name": "Chandima Dissanayaka",
+    "phone": "",
+    "table": "8"
+  },
+  {
+    "name": "Savindi Wimalarathna",
+    "phone": "",
+    "table": "9"
+  },
+  {
+    "name": "Hasindi Gamage",
+    "phone": "",
+    "table": "9"
+  },
+  {
+    "name": "Naveesha Kodikara",
+    "phone": "",
+    "table": "9"
+  },
+  {
+    "name": "Gayasha Kodikara",
+    "phone": "",
+    "table": "9"
+  },
+  {
+    "name": "Senadi Walawage",
+    "phone": "",
+    "table": "9"
+  },
+  {
+    "name": "Nethmini Madurapperuma",
+    "phone": "",
+    "table": "9"
+  },
+  {
+    "name": "Nethini Weerakoon",
+    "phone": "",
+    "table": "9"
+  },
+  {
+    "name": "Minuri Weerakoon",
+    "phone": "",
+    "table": "9"
+  },
+  {
+    "name": "Charlotte Cromie",
+    "phone": "",
+    "table": "9"
+  },
+  {
+    "name": "Jaishna Gungiah",
+    "phone": "",
+    "table": "9"
+  },
+  {
+    "name": "Ranumi Wanninayaka",
+    "phone": "",
+    "table": "10"
+  },
+  {
+    "name": "Nethaya Thennakoon",
+    "phone": "",
+    "table": "10"
+  },
+  {
+    "name": "Dinaya Mendis",
+    "phone": "",
+    "table": "10"
+  },
+  {
+    "name": "Thishen Wimalarathna",
+    "phone": "",
+    "table": "10"
+  },
+  {
+    "name": "Shenal Rathnayaka",
+    "phone": "",
+    "table": "10"
+  },
+  {
+    "name": "Binupa Ekanayaka",
+    "phone": "",
+    "table": "10"
+  },
+  {
+    "name": "Sanula Ahebadagedara",
+    "phone": "",
+    "table": "10"
+  },
+  {
+    "name": "Ometh",
+    "phone": "",
+    "table": "10"
+  },
+  {
+    "name": "Ragaf",
+    "phone": "",
+    "table": "10"
+  },
+  {
+    "name": "?",
+    "phone": "",
+    "table": "10"
+  },
+  {
+    "name": "Sumedha Rajakaruna",
+    "phone": "",
+    "table": "11"
+  },
+  {
+    "name": "Gayani Rajakaruna",
+    "phone": "",
+    "table": "11"
+  },
+  {
+    "name": "Athula Koralage",
+    "phone": "",
+    "table": "11"
+  },
+  {
+    "name": "Ruvini Koralage",
+    "phone": "",
+    "table": "11"
+  },
+  {
+    "name": "Kamal Kariyawasam",
+    "phone": "",
+    "table": "11"
+  },
+  {
+    "name": "Chamari Kariyawasam",
+    "phone": "",
+    "table": "11"
+  },
+  {
+    "name": "Hiran Kodikara",
+    "phone": "",
+    "table": "11"
+  },
+  {
+    "name": "Sajini Dilrukshi",
+    "phone": "",
+    "table": "11"
+  },
+  {
+    "name": "Jagath Wijesekara",
+    "phone": "",
+    "table": "11"
+  },
+  {
+    "name": "Gayathri Meegahakotuwa",
+    "phone": "",
+    "table": "11"
+  },
+  {
+    "name": "Mahesh Fernando",
+    "phone": "",
+    "table": "12"
+  },
+  {
+    "name": "Nimmi Fernando",
+    "phone": "",
+    "table": "12"
+  },
+  {
+    "name": "Gihan Walawage",
+    "phone": "",
+    "table": "12"
+  },
+  {
+    "name": "Ureshi Walawage",
+    "phone": "",
+    "table": "12"
+  },
+  {
+    "name": "Ananda Dikovita",
+    "phone": "",
+    "table": "12"
+  },
+  {
+    "name": "Niranji Wickranasinghe",
+    "phone": "",
+    "table": "12"
+  },
+  {
+    "name": "Sameera Thantreege",
+    "phone": "",
+    "table": "12"
+  },
+  {
+    "name": "Hasini Punchihewa",
+    "phone": "",
+    "table": "12"
+  },
+  {
+    "name": "Senith Amunugama",
+    "phone": "",
+    "table": "12"
+  },
+  {
+    "name": "?",
+    "phone": "",
+    "table": "12"
   }
 ];
